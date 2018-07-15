7793d28ab24307ee88d81119625723d12cc0a449
胡志方 <1583618895@qq.com>
2018-07-16 01:15:29 +0800

    Add files via upload

---
+++ sell/src/test/java/com/ipowertec/sell/SellApplicationTests.java
@@ -0,0 +1,22 @@
+package com.ipowertec.sell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SellApplicationTests {
+
+	private final Logger log = LoggerFactory.getLogger(SellApplicationTests.class);
+	
+	@Test
+	public void contextLoads() {
+		log.info("info .....");
+		log.error("error ....");
+		log.debug("debug ....");
+	}
+}
